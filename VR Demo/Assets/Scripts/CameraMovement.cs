76adb3cf114d6298516e3b957508ce40f09b99d3
Harsh Gupta <harshgupt91@gmail.com>
2018-12-28 18:44:24 +0530

    Added interactable object and platforms, object detection

---
+++ VR Demo/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Camera camLeft;
+    public Camera camRight;
+
+    public GameObject camParent;
+
+    private Quaternion rot;
+
+    private void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+        }
+        camParent.transform.rotation = Quaternion.Euler(90f, 90f, 0);
+        rot = new Quaternion(0, 0, 1, 0);
+    }
+
+    private void Update()
+    {
+        //camLeft.transform.localRotation = Quaternion.Euler(0, 90f, 180f) * rot;
+        camLeft.transform.localRotation = Input.gyro.attitude * rot;
+        camRight.transform.localRotation = Input.gyro.attitude * rot;
+    }
+
+
+
+}
---
+++ VR Demo/Assets/Scripts/ObjectDetection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectDetection : MonoBehaviour {
+
+    //Approximating the object looked at, is from the left camera
+    public Camera cam;
+
+    RaycastHit hit;
+
+    public Image loadFillLeft;
+    public Image loadFillRight;
+
+    //Bool to prevent multiple calls of the function within update, while looking at an object
+    bool lookingAtObject = false;
+	
+	void Update ()
+    {
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
+        {
+            //Debug.Log(lookingAtObject);
+            if(hit.collider.gameObject.tag == "InteractableObject" || hit.collider.gameObject.tag == "TeleportPlatform")
+            {
+                loadFillLeft.fillAmount += 0.01f;
+                loadFillRight.fillAmount += 0.01f;
+                if (!lookingAtObject)
+                {
+                    Debug.Log(hit.collider.gameObject.name);
+                    lookingAtObject = true;
+                }
+            }
+            else
+            {
+                loadFillLeft.fillAmount = 0f;
+                loadFillRight.fillAmount = 0f;
+                //Debug.Log(hit.collider.gameObject.name);
+                lookingAtObject = false;
+            }
+        }
+	}
+}
