cdc15feae4912a30087368be3b732ca21da2ab15
Harsh Gupta <harshgupt91@gmail.com>
2018-12-28 19:27:48 +0530

    Teleport function added

--- VR Demo/Assets/Scripts/ObjectDetection.cs
+++ VR Demo/Assets/Scripts/ObjectDetection.cs
@@ -4,40 +4,66 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ObjectDetection : MonoBehaviour {
+    
+    public Camera camLeft;
+    public Camera camRight;
 
-    //Approximating the object looked at, is from the left camera
-    public Camera cam;
+    public GameObject camParent;
+    public GameObject objectMenu;
 
     RaycastHit hit;
 
     public Image loadFillLeft;
     public Image loadFillRight;
-
-    //Bool to prevent multiple calls of the function within update, while looking at an object
-    bool lookingAtObject = false;
 	
 	void Update ()
     {
-        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
+        if(Physics.Raycast(camLeft.transform.position, camLeft.transform.forward, out hit) || Physics.Raycast(camRight.transform.position, camRight.transform.forward, out hit))
         {
-            //Debug.Log(lookingAtObject);
             if(hit.collider.gameObject.tag == "InteractableObject" || hit.collider.gameObject.tag == "TeleportPlatform")
             {
+                //Debug.Log(hit.collider.gameObject.name);
                 loadFillLeft.fillAmount += 0.01f;
                 loadFillRight.fillAmount += 0.01f;
-                if (!lookingAtObject)
+                if(loadFillLeft.fillAmount >= 1 || loadFillRight.fillAmount >= 1)
                 {
-                    Debug.Log(hit.collider.gameObject.name);
-                    lookingAtObject = true;
+                    if(hit.collider.gameObject.tag == "InteractableObject")
+                    {
+
+                    }
+                    else if(hit.collider.gameObject.tag == "TeleportPlatform")
+                    {
+                        TeleportToPlatform(hit.collider.gameObject.name);
+                    }
                 }
             }
             else
             {
                 loadFillLeft.fillAmount = 0f;
                 loadFillRight.fillAmount = 0f;
-                //Debug.Log(hit.collider.gameObject.name);
-                lookingAtObject = false;
             }
         }
 	}
+
+    void TeleportToPlatform(string platformName)
+    {
+        Vector3 newPos;
+        switch (platformName)
+        {
+            case "Platform1":
+                newPos = new Vector3(0, 7f, -10f);
+                camParent.transform.position = newPos;
+                break;
+            case "Platform2":
+                newPos = new Vector3(10f, 7f, 10f);
+                camParent.transform.position = newPos;
+                break;
+            case "Platform3":
+                newPos = new Vector3(-10f, 7f, 10f);
+                camParent.transform.position = newPos;
+                break;
+            default:
+                break;
+        }
+    }
 }
