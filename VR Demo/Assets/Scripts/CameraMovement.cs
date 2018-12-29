4b70c0e855c508ae7e5203781b5942160c9774fa
Harsh Gupta <harshgupt91@gmail.com>
2018-12-29 20:03:41 +0530

    Added textures, menu rotates to face camera

--- VR Demo/Assets/Scripts/CameraMovement.cs
+++ VR Demo/Assets/Scripts/CameraMovement.cs
@@ -17,17 +17,13 @@ public class CameraMovement : MonoBehaviour {
         {
             Input.gyro.enabled = true;
         }
-        camParent.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+        camParent.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
         rot = new Quaternion(0, 0, 1, 0);
     }
 
     private void Update()
     {
-        //camLeft.transform.localRotation = Quaternion.Euler(0, 90f, 180f) * rot;
         camLeft.transform.localRotation = Input.gyro.attitude * rot;
         camRight.transform.localRotation = Input.gyro.attitude * rot;
     }
-
-
-
 }
---
+++ VR Demo/Assets/Scripts/RotateMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMenu : MonoBehaviour {
+
+    public GameObject camParent;
+
+    private void Update()
+    {
+        transform.LookAt(camParent.transform);
+    }
+}
